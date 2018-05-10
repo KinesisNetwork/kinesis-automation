f092f1db333291cca931e9a44b44727633fc1f2c
Unknown <anita.coffey@abx.com>
2018-05-10 16:05:54 +1000

    Balances correctly updated transferring to existing account

--- src/specs/transfer.spec.ts
+++ src/specs/transfer.spec.ts
@@ -23,9 +23,41 @@ describe('Transfer', function () {
     const rootAccountBalanceAfterTransfer = await network.getAccountBalance(network.rootPublic)
     const newAccountBalance = await network.getAccountBalance(newAccount.publicKey())
 
-    debugger
-
     expect(rootAccountBalanceAfterTransfer).to.eql(expectedEndRootAccountBalance)
     expect(newAccountBalance).to.eql(transactionAmount)
+  })
+
+  it('Balances are updated correctly when transferring to an existing account', async () => {
+    const transactionAmount = 100
+    const newAccount = network.getNewKeypair()
+    const payableFee = await network.currentFee(transactionAmount)
+
+    // Activate account
+    await network.transferFunds(
+      network.rootPublic,
+      network.rootSecret,
+      newAccount.publicKey(),
+      transactionAmount,
+      true
+    )
+
+    const rootAccountBalance = await network.getAccountBalance(network.rootPublic)
+    const newAccountBalance = await network.getAccountBalance(newAccount.publicKey())
+
+    await network.transferFunds(
+      network.rootPublic,
+      network.rootSecret,
+      newAccount.publicKey(),
+      transactionAmount,
+      false
+    )
+    const rootAccountBalanceAfterTransfer = await network.getAccountBalance(network.rootPublic)
+    const newAccountBalanceAfterTransfer = await network.getAccountBalance(newAccount.publicKey())
+
+    const expectedEndRootBalance = rootAccountBalance - transactionAmount - payableFee
+    const expectedEndNewAccountBalance = newAccountBalance + transactionAmount
+
+    expect(rootAccountBalanceAfterTransfer).to.eql(expectedEndRootBalance)
+    expect(newAccountBalanceAfterTransfer).to.eql(expectedEndNewAccountBalance)
   })
 })
